a9d243eab4234397df5db68bdfbe512aa314c015
Sidharth Verma <sidharthverma@dailyobjects.com>
2025-07-27 20:14:54 +0530

    test add files

--- accounts_v2/communication.py
+++ accounts_v2/communication.py
@@ -153,6 +153,19 @@ def send_otp_email(email, otp, message):
     msg.send(fail_silently=False)
     return True
 
+def send_otp_email(email, otp, message):
+    from_email = formataddr(('Studentpeeps', settings.DEFAULT_FROM_EMAIL)) # Need to ask ayush about sender name
+    subject = f"{otp} is your Studentpeeps passcode."
+    msg = EmailMessage(
+                subject,
+                message,
+                from_email,
+                [email],
+            )
+    msg.content_subtype = "html"  # Main content is now text/html
+    msg.send(fail_silently=False)
+    return True
+
 
 def send_welcome_email(subject, email, message):
     from_email = formataddr(('Studentpeeps', settings.DEFAULT_FROM_EMAIL))
