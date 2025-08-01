ad644a22e4b62c91e9e2dc97275e62c9ca26722a
Sidharth Verma <sidharthverma@dailyobjects.com>
2025-08-01 17:22:36 +0530

    add filter for register code

--- accounts_v2/admin.py
+++ accounts_v2/admin.py
@@ -1,10 +1,11 @@
 # admin.py
 from django.contrib import admin
-from .models import Register, UnVerified, AbandonedSignup
+from .models import Register, UnVerified, AbandonedSignup, UnVerifiedIdUpload
 
 class RegisterAdmin(admin.ModelAdmin):
-    list_display = ('user_name', 'user_email', 'phone', 'created_at')
+    list_display = ('user_name', 'user_email', 'phone', 'created_at', 'is_verified')
     readonly_fields = ('created_at',)
+    list_filter = ('is_verified',)  # <-- Add this line for filtering by is_verified
 
     def user_email(self, obj):
         return obj.user.email
@@ -17,4 +18,5 @@ class RegisterAdmin(admin.ModelAdmin):
 admin.site.register(Register, RegisterAdmin)
 
 admin.site.register(UnVerified)
+admin.site.register(UnVerifiedIdUpload)
 admin.site.register(AbandonedSignup)
