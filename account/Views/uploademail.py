7abb8bce15e9fbf67f6612c03519f3d58aaeade0
Sidharth Verma <sidharthverma@dailyobjects.com>
2025-08-06 14:43:29 +0530

    add button and change sub line

--- account/Views/uploademail.py
+++ account/Views/uploademail.py
@@ -61,31 +61,44 @@ class RejectUploadEmailUser(View):
     def post(self, request):
         messages = [] 
         email = request.POST['email']
+        register = None
+        user = None
+
         try:
             register = Register.objects.get(Q(user__username=email) | Q(institution_email=email))
             user = register.user
             user.backend = 'django.contrib.auth.backends.ModelBackend'     
-            messages.append("User is rejected!") 
+            messages.append("User is rejected from Register!") 
         except Register.DoesNotExist:
             try:
                 register = UnVerifiedIdUpload.objects.get(Q(user__username=email) | Q(email=email))
-                messages.append("User is rejected!")             
+                user = register.user
+                messages.append("User is rejected from UnVerifiedIdUpload!")             
             except UnVerifiedIdUpload.DoesNotExist:
                 messages.append("User not found!")
                 return render(request, 'uploademail.html', {'messages': messages})
-            return render(request, 'uploademail.html', {'messages': messages})
 
-        RejectedUser = RejectedUsers(user=register.user, phone=register.phone, firstname=register.firstname, lastname=register.lastname, gender=register.gender, birthday=register.birthday, collegeId=register.collegeId)
+        RejectedUser = RejectedUsers(
+            user=user,
+            phone=register.phone,
+            firstname=register.firstname,
+            lastname=register.lastname,
+            gender=register.gender,
+            birthday=register.birthday,
+            collegeId=register.collegeId
+        )
         RejectedUser.save()
+
         user.is_active = False
         user.save()
         register.delete()
 
         message = render_to_string('emailers/user_rejection_email_body.html', {'fname': register.firstname})
-        send_welcome_email(subject=f"Your student identification has been rejected", email=email, message=message)
+        send_welcome_email(subject="Action Needed: Reapply for Studentpeeps Verification", email=email, message=message)
 
         messages.append("Email sent to user about rejection and user is now inactive.")
         return render(request, 'uploademail.html', {'messages': messages})
+
 
 class VerifyUploadPhoneUsers(View):
     def post(self, request):
