b9403a3378656d376dd573eb613ed99857f9e78f
Sidharth Verma <sidharthverma@dailyobjects.com>
2025-09-06 19:18:46 +0530

    add table in redemed codes too

--- brands_v2/admin.py
+++ brands_v2/admin.py
@@ -28,9 +28,34 @@ class OfferSEOAdmin(admin.ModelAdmin):
 
 @admin.register(RedeemedCodes)
 class RedeemedCodesAdmin(admin.ModelAdmin):
-    list_display = ('offer_custom_id', 'created_at', 'updated_at')
+    list_display = ('offer_custom_id', 'codes_count', 'created_at', 'updated_at')
     search_fields = ('offer_custom_id',)
     list_per_page = 50
+    readonly_fields = ("formatted_redeemed_codes_html",)
+
+    def codes_count(self, obj):
+        return len(obj.redeemed_codes)
+    codes_count.short_description = "Total Codes"
+
+    def formatted_redeemed_codes_html(self, obj):
+        if not obj.redeemed_codes:
+            return "-"
+
+        rows = "".join(
+            [f"<tr><td>{list(code.keys())[0]}</td><td>{list(code.values())[0]}</td></tr>" for code in obj.redeemed_codes]
+        )
+        table = f"""
+        <table border="1" cellpadding="5" cellspacing="0" style="border-collapse: collapse;">
+            <tr style="background:#000000; color:#fff; font-weight:bold;">
+                <th style="width: 300px;">Redeemed Code</th>
+                <th style="width: 250px;">Redeemed At (UTC)</th>
+            </tr>
+            {rows}
+        </table>
+        """
+        return format_html(table)
+
+    formatted_redeemed_codes_html.short_description = "Redeemed Codes Dashboard"
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
